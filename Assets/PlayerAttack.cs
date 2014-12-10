1768c267cadfc0c11c928b59532fe2c67d2f3a6d
ryo miyake <ryoujubeat0916@gmail.com>
2014-12-10 13:47:48 +0900

    add PlayerAttack

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class PlayerAttack : MonoBehaviour {
+
+
+	Controller controller = new Controller();
+	public int FingerCount;
+	public Texture OnePlayercrosshairImage;
+	public Texture TwoPlayercrosshairImage;
+	public AudioClip FireSE;
+	public AudioClip IceSE;
+	public AudioClip SanderSE;
+	public GameObject targetGameObject;
+	
+	
+	void Start() { 
+		
+		}
+	
+	void Update() {
+	
+		Frame frame = controller.Frame();
+		FingerCount = frame.Fingers.Count;
+		
+	}
+
+
+	
+    
+
+	void OnGUI()
+{	
+	if (controller.IsConnected) {
+		Frame frame = controller.Frame();
+		
+
+		HandList hands = frame.Hands;
+		Hand firstHand = hands[0];
+		Hand secondHand = hands[1];
+		/*InteractionBox iBox = frame.InteractionBox;
+		Vector handCenter = firstHand.PalmPosition;
+		Vector normalizedPosition = iBox.NormalizePoint(handCenter);
+		
+		if (Input.GetButtonDown("Fire1")) {
+		Ray ray;
+    	RaycastHit hit;
+    	
+    	ray = Camera.main.ScreenPointToRay(normalizedPosition);
+    	
+    	 if (Physics.Raycast(ray, out hit, 1000)) {
+        targetGameObject = hit.collider.gameObject;
+    } else {
+        targetGameObject = null;
+    }
+    }*/
+		
+
+
+		/*InteractionBox iBox = frame.InteractionBox;
+
+		Vector firstHandnormalizedPosition = iBox.NormalizePoint(firstHand.PalmPosition);
+		Vector secondHandnormalizedPosition = iBox.NormalizePoint(secondHand.PalmPosition);*/
+
+
+		float firsthandX = firstHand.PalmPosition.x + 300;
+		float firsthandY = -firstHand.PalmPosition.y + 600;
+		
+		float secondhandX = secondHand.PalmPosition.x + 900;
+		float secondhandY = -secondHand.PalmPosition.y + 600;
+		
+		
+		
+		
+
+		GUI.DrawTexture (new Rect (firsthandX + (OnePlayercrosshairImage.width / 2),
+		                           firsthandY - (OnePlayercrosshairImage.height / 2),
+		                           OnePlayercrosshairImage.width,
+		                           OnePlayercrosshairImage.height), OnePlayercrosshairImage);
+		                           
+		GUI.DrawTexture (new Rect (secondhandX + (TwoPlayercrosshairImage.width / 2),
+		                           secondhandY - (TwoPlayercrosshairImage.height / 2),
+		                           TwoPlayercrosshairImage.width,
+		                           TwoPlayercrosshairImage.height), TwoPlayercrosshairImage);		
+
+			}
+
+}
+}
--- Assets/Title/TitleMenu.js
+++ Assets/Title/TitleMenu.js
@@ -2,9 +2,12 @@
 
 function OnGUI()
 {
-	if (GUI.Button(Rect(350, 280, 400, 130), "Start"))
+	if (GUI.Button(Rect(150, 380, 200, 130), "Start"))
 	{
 		Application.LoadLevel("FairyForest");
         
+	}
+	if (GUI.Button(Rect(450, 380, 200, 130), "Game End")) {
+	Application.Quit();
 	}
 }
